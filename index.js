ff98284862ec63689c9075748dfad96fd94697cd
njorogecharles439@gmail.com <njorogecharles439@gmail.com.>
2024-10-05 07:19:15 +0300

    sole

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+
 // Code your solution here
+
+function totalBatteries (batteryBatches){
+
+    return totalBatteries = 4 + 5 + 3 + 4 + 4 + 6 + 5;
+
+}
+console.log(totalBatteries(batteryBatches));
+
+
+
+
